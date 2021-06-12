1cb2d3dcc0f9624c18968d015be82230a635cdd4
Tyler Weeres <tweeres04@gmail.com>
2021-06-11 18:50:07 -0700

    Add a timezone to the prompt queueing api endpoint

--- pages/api/queue-prompt-notifications.ts
+++ pages/api/queue-prompt-notifications.ts
@@ -1,3 +1,5 @@
+process.env.TZ = 'America/Vancouver'
+
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import _ from 'lodash'
